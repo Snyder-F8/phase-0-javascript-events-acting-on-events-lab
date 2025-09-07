fd6f0ad575a4c3b66c49fbaee40978aa09ec04c2
Snyder-F8 <fridahnzomo857@gmail.com>
2025-09-07 14:02:36 +0300

    Created Dodger movement

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left < 360) { // assuming game width = 400px and dodger width = 40px
+    dodger.style.left = `${left + 1}px`;
+  }
+}
